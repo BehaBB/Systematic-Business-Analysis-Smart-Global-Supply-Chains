fa8395cba793873ff75aa448b6e45b63d1c9b7fa
BehaB <91378877+BehaBB@users.noreply.github.com>
2025-10-23 13:51:26 +0800

    Delete 3-Modelling-And-Design/02-data-model.sql

--- 3-Modelling-And-Design/02-data-model.sql
+++
@@ -1,8 +0,0 @@
--- Patients with cultural attributes
-CREATE TABLE patients (
-    id UUID PRIMARY KEY,
-    first_name VARCHAR(100),
-    ethnicity VARCHAR(50),
-    preferred_language VARCHAR(10) DEFAULT 'buryat',
-    traditional_medicine_acceptance INTEGER
-);
